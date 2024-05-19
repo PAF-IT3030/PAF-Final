5b0bf8f4cf4c7016a4fb79961c37b94a98f10477
SubasingheN <100112786+SubasingheN@users.noreply.github.com>
2024-05-19 09:23:21 +0530

    Add files via upload

---
+++ usersmanagementsystem/src/main/java/com/nuwan/usersmanagementsystem/repository/UsersRepo.java
@@ -0,0 +1,11 @@
+package com.nuwan.usersmanagementsystem.repository;
+
+import com.nuwan.usersmanagementsystem.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
